ade739ff6e2fbd40827ea6e9fd6dab58f244dcbf
CosminCAS123 <cosmingamer2004@gmail.com>
2024-08-26 02:29:19 +0300

    Add project files.

---
+++ ShopSphere/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ShopSphere.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ShopSphere/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace ShopSphere.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
