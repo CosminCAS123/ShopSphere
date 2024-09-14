dd0b4ade8624064195d6a7b46151d8a02491551f
CosminCAS123 <cosmingamer2004@gmail.com>
2024-09-15 01:34:52 +0300

    stack navigation is empty

--- ShopSphere/ViewModels/ThirdRegisterVM.cs
+++ ShopSphere/ViewModels/ThirdRegisterVM.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml.MarkupExtensions;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
@@ -27,7 +28,8 @@ namespace ShopSphere.ViewModels
         private string success_or_error_text;
         private ISolidColorBrush error_color;
 
-        
+        private double GRID_OPACITY_AFTER_REGISTERED = 0.3;
+     
         private readonly string default_pic = "avares://ShopSphere/Assets/DefaultPFP.png";
         private string username;
         private string image_extension;
@@ -45,8 +47,17 @@ namespace ShopSphere.ViewModels
         private Stream ImageStream = null;
         private IUserRepository userRepository;
         private bool isLoading = false;
-        private bool can_try_again;
-    //    public bool IsErrorTextVisible { get => this.is_error_visible; set => this.RaiseAndSetIfChanged(ref this.is_error_visible, value); }
+        private bool can_try_again = true;
+        private string must_select_pfp = "You must select a profile picture.";
+        private string invalid_user = "Username has invalid format.";
+        private string empty_username  = "Username cannot be empty.";
+        private Grid grid;
+public Grid GridControl { get => this.grid; set => this.RaiseAndSetIfChanged(ref this.grid, value); }
+        private bool isDoneVisible;
+        public bool IsDoneVisible { get => this.isDoneVisible; set => this.RaiseAndSetIfChanged(ref this.isDoneVisible, value); }
+        public ReactiveCommand<Unit, Unit> BackToLoginCommand { get; set; }
+        private bool IS_REGISTERED = false;
+        //    public bool IsErrorTextVisible { get => this.is_error_visible; set => this.RaiseAndSetIfChanged(ref this.is_error_visible, value); }
         public ThirdRegisterVM(IAuthNavigationService navigation_service , IUserRepository user_repository) : base(navigation_service)
         {
             this.userRepository = user_repository;
@@ -54,7 +65,7 @@ namespace ShopSphere.ViewModels
 
              this.userpfp_path = Path.Combine(projectRootPath, "ShopSphere", "Assets", "UserPFP");
 
-         
+            this.IsDoneVisible = false;
 
             this.SelectImageCommand = ReactiveCommand.CreateFromTask<UserControl>(selectImageCommand);
           
@@ -62,7 +73,7 @@ namespace ShopSphere.ViewModels
 
             this.FinishRegisterCommand = ReactiveCommand.CreateFromTask(finish_register_command);
             this.SuccessOrErrorText = string.Empty;
-
+            this.BackToLoginCommand = ReactiveCommand.Create(() => this.navigationService.AuthRegisterFinished());
             ObserveUsername();
             
         }
@@ -70,10 +81,7 @@ namespace ShopSphere.ViewModels
         {
             this.WhenAnyValue(x => x.Username).Subscribe(x =>
             {
-                /*username needs - length at least 6
-                                 - one uppercase letter
-                                 - one lowercase letter 
-                                 - one digit */
+              
                 string pattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
                 if (!string.IsNullOrEmpty(x) && !Regex.IsMatch(x, pattern)) this.IsErrorVisible = true;
                 else this.IsErrorVisible = false;
@@ -82,18 +90,13 @@ namespace ShopSphere.ViewModels
 
             });
         }
-     /*private void SetUser()
+     private void goToLogin()
         {
-            navigationService.RegisteredUser.Age = 19;
-            navigationService.RegisteredUser.EmailAdress = "cosmingamer2004@gmail.com";
-            navigationService.RegisteredUser.FirstName = "Cosmin";
-            navigationService.RegisteredUser.LastName = "Andrei";
-            navigationService.RegisteredUser.PasswordHash = "fsjhfsasjaf23831139fnasdsankksaf19021439";
-            navigationService.RegisteredUser.PhoneNumber = "+40741039009";
-            navigationService.RegisteredUser.SecurityPIN = "1234";
-           
-
-        }*/
+            this.IS_REGISTERED = true;
+            this.GridControl.Opacity = this.GRID_OPACITY_AFTER_REGISTERED;
+            this.GridControl.IsEnabled = false;
+            this.IsDoneVisible = true;
+        }
      private void SetErrorText(string message , IImmutableSolidColorBrush color)
         {
             this.SuccessOrErrorText = message;
@@ -104,113 +107,129 @@ namespace ShopSphere.ViewModels
         {
             await Task.Delay(3000);
             this.can_try_again = true;
+            DisableErrorText();
         }
         private async Task finish_register_command()
         {
-            if (can_try_again)
+            if (!IS_REGISTERED)
             {
-                if (!string.IsNullOrEmpty(this.Username))
+                if (can_try_again)
                 {
-                    if (!this.IsErrorVisible)
+                    if (!string.IsNullOrEmpty(this.Username))
                     {
-                        if (this.ImageStream is not null)
+                        if (!this.IsErrorVisible)
                         {
-                            //check if username exists in db
-                            if (await this.userRepository.GetUserByUsernameAsync(Username) is not null) // already exists
+                            if (this.ImageStream is not null)
                             {
+                                //check if username exists in db
+                                if (await this.userRepository.GetUserByUsernameAsync(Username) is not null) // already exists
+                                {
 
-                                SetErrorText(ErrorResources.Register.UsernameAlreadyExists, Brushes.Red);
-                                //wait 3 seconds
-                                this.can_try_again = false;
-                                _ = Task.Run(wait_a_bit);
-                                return;
+                                    SetErrorText(ErrorResources.Register.UsernameAlreadyExists, Brushes.Red);
+                                    //wait 3 seconds
+                                    this.can_try_again = false;
+
+                                    _ = Task.Run(wait_a_bit);
+
+
+                                    return;
+                                }
+                                else
+                                {
+                                    //ADD PFP TO PROJECT
+
+
+                                    var file_name = $"{this.Username}pfp{this.image_extension}";
+
+
+                                    var full_img_path = Path.Combine(userpfp_path, file_name);
+
+                                    await using var fileStream = File.Create(full_img_path);
+
+
+
+                                    // Ensure the image stream is at the beginning
+                                    this.ImageStream.Seek(0, SeekOrigin.Begin);
+
+                                    // Copy the image stream to the file
+                                    await this.ImageStream.CopyToAsync(fileStream);
+
+
+
+                                    //REGISTER WHOLE USER
+
+                                    this.navigationService.RegisteredUser.ProfilePictureUrl = full_img_path;
+                                    this.navigationService.RegisteredUser.Username = this.Username;
+
+                                    //ADD USER TO DATABASE
+
+                                    await this.userRepository.AddUserAsync(navigationService.RegisteredUser);
+
+                                    //REGISTERED SUCCESSFULLY
+                                    SetErrorText(ErrorResources.Register.RegisteredSuccessfully, Brushes.YellowGreen);
+
+                                    goToLogin();
+
+                                }
                             }
-                            else
+                            else //image not entered
                             {
-                                //ADD PFP TO PROJECT
-
-
-                                var file_name = $"{this.Username}pfp{this.image_extension}";
-
-
-                                var full_img_path = Path.Combine(userpfp_path, file_name);
-
-                                await using var fileStream = File.Create(full_img_path);
-
-
-
-                                // Ensure the image stream is at the beginning
-                                this.ImageStream.Seek(0, SeekOrigin.Begin);
-
-                                // Copy the image stream to the file
-                                await this.ImageStream.CopyToAsync(fileStream);
-
-
-
-                                //REGISTER WHOLE USER
-
-                                this.navigationService.RegisteredUser.ProfilePictureUrl = full_img_path;
-                                this.navigationService.RegisteredUser.Username = this.Username;
-
-                                //ADD USER TO DATABASE
-
-                                await this.userRepository.AddUserAsync(navigationService.RegisteredUser);
-
-                                //Show user registered 
-                                SetErrorText(ErrorResources.Register.RegisteredSuccessfully, Brushes.YellowGreen);
                                 this.can_try_again = false;
-                              
+
+                                SetErrorText(this.must_select_pfp, Brushes.Red);
+                                _ = wait_a_bit();
+
                             }
+
                         }
-                        else //image not entered
+
+                        else //invalid username 
                         {
+                            this.can_try_again = false;
+                            SetErrorText(this.invalid_user, Brushes.Red);
+                            _ = wait_a_bit();
 
-                            this.SuccessOrErrorText = "You must select a profile picture.";
-                            this.ErrorColor = Brushes.Red;
                         }
+                    }
+                    else //username null
+                    {
+                        this.can_try_again = false;
+                        SetErrorText(this.empty_username, Brushes.Red);
+                        _ = wait_a_bit();
 
                     }
 
-                    else //invalid username 
-                    {
-                        this.SuccessOrErrorText = "Username has invalid format.";
-                        this.ErrorColor = Brushes.Red;
-                    }
-                }
-                else //username null
-                {
-                    this.SuccessOrErrorText = "Username cannot be empty.";
-                    this.ErrorColor = Brushes.Red;
-                }
 
-
+                }
             }
         }
         private async Task selectImageCommand(UserControl registerView)
         {
-            var topLevel = TopLevel.GetTopLevel(registerView);
-            if (topLevel?.StorageProvider is not null)
+            if (!IS_REGISTERED)
             {
-                var files = await topLevel.StorageProvider.OpenFilePickerAsync(new Avalonia.Platform.Storage.FilePickerOpenOptions
+                var topLevel = TopLevel.GetTopLevel(registerView);
+                if (topLevel?.StorageProvider is not null)
                 {
-                    Title = "Select an image",
-                    AllowMultiple = false,
-                    FileTypeFilter = new[]
-                      {
+                    var files = await topLevel.StorageProvider.OpenFilePickerAsync(new Avalonia.Platform.Storage.FilePickerOpenOptions
+                    {
+                        Title = "Select an image",
+                        AllowMultiple = false,
+                        FileTypeFilter = new[]
+                          {
                      new FilePickerFileType("PNG, JPG, JPEG") { Patterns = new[] { "*.png", "*.jpg", "*.jpeg" } }
                 }
-                });
+                    });
 
-                if (files.Count > 0)
-                {
-                    var stream = await files[0].OpenReadAsync();
-                    
-                    this.ImageStream = stream;
-                    this.SelectedImage = new Bitmap(stream);
-                    this.image_extension = Path.GetExtension(files[0].Name);
+                    if (files.Count > 0)
+                    {
+                        var stream = await files[0].OpenReadAsync();
+
+                        this.ImageStream = stream;
+                        this.SelectedImage = new Bitmap(stream);
+                        this.image_extension = Path.GetExtension(files[0].Name);
+                    }
                 }
             }
-
 
         }
     }
