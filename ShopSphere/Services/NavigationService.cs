a12ac67c3555ef720f3aef5ee06e758da53e6023
CosminCAS123 <cosmingamer2004@gmail.com>
2024-09-15 15:13:56 +0300

    registration process finished

--- ShopSphere/Services/NavigationService.cs
+++ ShopSphere/Services/NavigationService.cs
@@ -16,21 +16,28 @@ namespace ShopSphere.Services
         private readonly IServiceProvider serviceProvider;
         private Stack<AuthViewModelBase> navigationStack;
         private User registered_user;
+        private IServiceScope registrationScope;
         private AuthViewModelBase auth_content;
         public User RegisteredUser { get => this.registered_user; set => this.RaiseAndSetIfChanged(ref this.registered_user, value); }
         public AuthViewModelBase AuthContent { get => this.auth_content; set => this.RaiseAndSetIfChanged(ref this.auth_content, value); }
         
         public void AuthRegisterFinished()
         {
+
             for (int i = 0; i < 3; i++) this.navigationStack.Pop();
+            this.registrationScope.Dispose();
+            
+            this.AuthContent = navigationStack.Peek();
+            this.registrationScope = serviceProvider.CreateScope();
         }
         public void AuthNavigateTo<TViewModel>() where TViewModel : AuthViewModelBase
         {
+            
+                var vm = registrationScope.ServiceProvider.GetService<TViewModel>()!;
+                navigationStack.Push(vm);
 
-            var vm = serviceProvider.GetService<TViewModel>()!;
-            navigationStack.Push(vm);
-
-            this.AuthContent = vm;
+                this.AuthContent = vm;
+            
         }
         public void AuthNavigateBack() {
 
@@ -42,12 +49,14 @@ namespace ShopSphere.Services
         
         public NavigationService(IServiceProvider service_provider)
         {
+           
             this.serviceProvider = service_provider;
             this.RegisteredUser = new User();
             this.navigationStack = new Stack<AuthViewModelBase>();
-            
-           
-        
+            this.registrationScope = serviceProvider.CreateScope();
+
+
+
         }
        
     }
